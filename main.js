eb3bd4646ae9e4682e4e5f08613bb1153ce74344
Creed Camp <creedomed@gmail.com>
2020-09-17 09:02:23 -0700

    turning in

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 //const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-function add(event) {
-  event.preventDefault()
+//the above would be used toward making the hard mode
+//this first function makes add and has it grab the two values entered and put them toward the "answer" inside of the html.
+//the functions following it do the same, but for subtraction, multiplication, and division.
+function add(e) {
+  e.preventDefault()
   const nums1 = document.querySelector("#nums1").value
   const nums2 = document.querySelector("#nums2").value
   let answer = document.querySelector("#answer")
   return (answer.innerHTML = Number(num1) + Number(num2))
 }
-document.querySelector("#add").addEventListener("#click", add)
+document.querySelector("#add").addEventListener("click", add)
 function subtract(e) {
   e.preventDefault()
   const num1 = document.querySelector("#num1").value
@@ -15,7 +17,7 @@ function subtract(e) {
   let answer = document.querySelector("#answer")
   return (answer.innerHTML = Number(num1) - Number(num2))
 }
-document.querySelector("#sub").addEventListener("#click", subtract)
+document.querySelector("#sub").addEventListener("click", subtract)
 function multiply(e) {
   e.preventDefault()
   const num1 = document.querySelector("#num1").value
@@ -23,7 +25,7 @@ function multiply(e) {
   let answer = document.querySelector("#answer")
   return (answer.innerHTML = Number(num1) * Number(num2))
 }
-document.querySelector("#mult").addEventListener("#click", multiply)
+document.querySelector("#mult").addEventListener("click", multiply)
 function divide(e) {
   e.preventDefault()
   const num1 = document.querySelector("#num1").value
@@ -31,4 +33,4 @@ function divide(e) {
   let answer = document.querySelector("#answer")
   return (answer.innerHTML = Number(num1) / Number(num2))
 }
-document.querySelector("#divi").addEventListener("#click", divide)
+document.querySelector("#divi").addEventListener("click", divide)
